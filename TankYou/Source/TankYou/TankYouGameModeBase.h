22de5aa7221bc124e824482b03729b41eb70ed7e
lk <leedakent123+git@hotmail.com>
2017-11-19 21:44:46 +0000

    created c++ project that can be built with vs2017

---
+++ TankYou/Source/TankYou/TankYouGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankYouGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKYOU_API ATankYouGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
